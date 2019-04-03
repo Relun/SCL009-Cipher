adfcbcec5cec7bf9b174ceac2b959415b23625dd
Relun <claudia.araya.ro@gmail.com>
2019-04-03 20:48:53 -0300

    agregando cifrado con minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,20 @@
 window.cipher = {
   encode: (offset, texto) => {
-    let result="";
-    for(let i=0; i<texto.length; i++){
+    let result=""; //variable con string vacío para guardar mi resultado
+    
+    for(let i=0; i<texto.length; i++){ //for sirve para recorrer las letras del texto que ingresa el usuario
+      let asciiNumber = texto.charCodeAt(i); //variable toma la letra y la pasa a cod ascii
 
-      if(texto.charCodeAt(i) >=65 && texto.charCodeAt(i) <=90);
-      let formula=(texto.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-      result += String.fromCharCode(formula);
+      if(asciiNumber >=65 && asciiNumber <=90){//if para condicion de mayusculas en ascii
+       cipheroffset=(asciiNumber - 65 + parseInt(offset)) % 26 + 65;//formula con el offset como parametro para que se corran las letras
+      }
+
+      else if(asciiNumber >=97 && asciiNumber <=122){
+       cipheroffset=(asciiNumber - 97 + parseInt(offset)) % 26 + 97;//parseint: toma una cadena de texto(string) y retorna su valor numerico
+
+      }
+      
+      result += String.fromCharCode(cipheroffset);//tomo cod ascii del texto cifrado y lo paso a string, asi el resultado lo entrega en string
 
       } 
 
